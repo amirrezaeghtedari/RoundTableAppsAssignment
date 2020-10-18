305e8876b69e9edfd28ff807f7b9ed17a750a59c
Amirreza Eghtedari <eghtedari.amir@gmail.com>
2020-10-18 16:58:57 +0330

    Remove Main.storyboard.

---
+++ RoundTableAppsAssignment/Entities/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  RoundTableAppsAssignment
+//
+//  Created by Amirreza Eghtedari on 7/27/1399 AP.
+//
+
+import Foundation
+
+struct Country {
+	
+	let name: String
+}
---
+++ RoundTableAppsAssignment/Scenes/Main/Composer/MainComposer.swift
@@ -0,0 +1,18 @@
+//
+//  MainComposer.swift
+//  RoundTableAppsAssignment
+//
+//  Created by Amirreza Eghtedari on 7/27/1399 AP.
+//
+
+import UIKit
+
+class MainComposer {
+	
+	func makeModule() -> MainViewController {
+		
+		let mainViewController = MainViewController()
+
+		return mainViewController
+	}
+}
