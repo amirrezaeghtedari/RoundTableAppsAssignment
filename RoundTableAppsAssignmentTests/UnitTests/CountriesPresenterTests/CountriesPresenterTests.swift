9daac427518801e57b86c749946b43ce0070840d
Amirreza Eghtedari <eghtedari.amir@gmail.com>
2020-10-23 18:41:25 +0330

    Rename Countries folder to CountriesList.

---
+++ RoundTableAppsAssignmentTests/UnitTests/CountriesPresenterTests/CountriesPresenterTests.swift
@@ -0,0 +1,25 @@
+//
+//  CountriesPresenterTests.swift
+//  RoundTableAppsAssignmentTests
+//
+//  Created by Amirreza Eghtedari on 8/2/1399 AP.
+//
+
+import XCTest
+@testable import RoundTableAppsAssignment
+
+class CountriesPresenterTests: XCTestCase {
+
+	
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+}
